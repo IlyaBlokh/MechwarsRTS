97bb5a85a07aae2f49cd85938d100887ca6a044a
Ilya Blokh <ilyablokh@gmail.com>
2022-02-17 23:18:00 +0500

    spawn units by click

---
+++ Assets/Scripts/Buildings/UnitSpawnerBuilding.cs
@@ -0,0 +1,36 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Buildings
+{
+    public class UnitSpawnerBuilding : NetworkBehaviour, IPointerClickHandler
+    {
+        [SerializeField]
+        private GameObject _laserTankUnitPrefab;
+        [SerializeField]
+        private Transform _spawnPoint;
+
+        #region server
+        [Command]
+        private void CmdSpawnLaserTankUnit()
+        {
+            var _laserTankUnit = Instantiate(_laserTankUnitPrefab,
+               _spawnPoint.position,
+               _spawnPoint.rotation);
+            NetworkServer.Spawn(_laserTankUnit, connectionToClient);
+        }
+        #endregion
+
+        #region client
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button != PointerEventData.InputButton.Left) return;
+            if (!hasAuthority) return;
+            CmdSpawnLaserTankUnit();
+        }
+        #endregion
+    }
+}
--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -1,9 +1,21 @@
 using Mirror;
+using UnityEngine;
 
 namespace Networking
 {
     public class RTSNetworkManager : NetworkManager
     {
+        [SerializeField]
+        private GameObject _unitSpawnerBuildingPrefab;
 
+        public override void OnServerAddPlayer(NetworkConnection conn)
+        {
+            base.OnServerAddPlayer(conn);
+            var _playerUnitSpawner = Instantiate(
+                        _unitSpawnerBuildingPrefab,
+                        conn.identity.transform.position,
+                        conn.identity.transform.rotation);
+            NetworkServer.Spawn(_playerUnitSpawner, conn);
+        }
     }
 }
