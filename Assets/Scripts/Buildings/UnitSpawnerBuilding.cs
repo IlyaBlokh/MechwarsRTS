ad8a19bb21ae0fbe005327fa2238399879b9ff06
Ilya Blokh <ilyablokh@gmail.com>
2022-02-24 01:38:06 +0500

    multiselect

--- Assets/Scripts/Buildings/UnitSpawnerBuilding.cs
+++ Assets/Scripts/Buildings/UnitSpawnerBuilding.cs
@@ -1,6 +1,4 @@
 using Mirror;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
--- Assets/Scripts/Units/UnitSelectionHandler.cs
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 namespace Units
@@ -13,10 +14,14 @@ namespace Units
         private RectTransform selectionFrame;
         [SerializeField]
         private LayerMask layerMask;
-        
+
+        private RTSPlayer player;
         private List<Unit> selectedUnits = new List<Unit>();
         private Camera mainCamera;
         private Vector2 startSelectionPosition;
+        private Vector2 mousePos;
+        private float selectionWidth;
+        private float selectionHeight;
 
         public LayerMask LayerMask { get => layerMask; }
         public List<Unit> SelectedUnits { get => selectedUnits; }
@@ -27,11 +32,16 @@ namespace Units
             mainCamera = Camera.main;
         }
 
+        private void Start()
+        {
+            player = NetworkClient.connection.identity.GetComponent<RTSPlayer>();
+        }
+
         private void Update()
         {
             if (Mouse.current.leftButton.wasPressedThisFrame)
             {
-                InitSelection(); 
+                InitSelection();
             }
             else
             if (Mouse.current.leftButton.isPressed)
@@ -51,30 +61,53 @@ namespace Units
             selectedUnits.Clear();
             selectionFrame.gameObject.SetActive(true);
             startSelectionPosition = Mouse.current.position.ReadValue();
+            UpdateSelection();
         }
 
         private void UpdateSelection()
         {
-            var mousePos = Mouse.current.position.ReadValue();
-            var width = mousePos.x - startSelectionPosition.x;
-            var height = mousePos.y - startSelectionPosition.y;
-            selectionFrame.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
-            selectionFrame.anchoredPosition = startSelectionPosition + new Vector2(width / 2, height / 2);
+            mousePos = Mouse.current.position.ReadValue();
+            selectionWidth = mousePos.x - startSelectionPosition.x;
+            selectionHeight = mousePos.y - startSelectionPosition.y;
+            selectionFrame.sizeDelta = new Vector2(Mathf.Abs(selectionWidth), Mathf.Abs(selectionHeight));
+            selectionFrame.anchoredPosition = startSelectionPosition + new Vector2(selectionWidth / 2, selectionHeight / 2);
         }
 
         private void ApplySelection()
         {
             selectionFrame.gameObject.SetActive(false);
-            if (!hasAuthority) return;
 
-            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-            if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) return;
+            if (selectionFrame.sizeDelta.magnitude < float.Epsilon)
+            {
+                if (!hasAuthority) return;
 
-            if (!hit.collider.TryGetComponent(out Unit unit)) return;
+                Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+                if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) return;
 
-            selectedUnits.Add(unit);
-            selectedUnits.ForEach(unit => unit.Select());
+                if (!hit.collider.TryGetComponent(out Unit unit)) return;
+
+                selectedUnits.Add(unit);
+                selectedUnits.ForEach(unit => unit.Select());
+            }
+            else
+            {
+                Vector2 leftBottomCorner = selectionFrame.anchoredPosition - selectionFrame.sizeDelta / 2;
+                Vector2 rightTopCorner = selectionFrame.anchoredPosition + selectionFrame.sizeDelta / 2;
+                player.Units.ForEach(unit => {
+                    var screenPosition = mainCamera.WorldToScreenPoint(unit.transform.position);
+                    if (screenPosition.x > leftBottomCorner.x &&
+                        screenPosition.x < rightTopCorner.x &&
+                        screenPosition.y > leftBottomCorner.y &&
+                        screenPosition.y < rightTopCorner.y)
+                    {
+                        selectedUnits.Add(unit);
+                        unit.Select();
+                        //selectedUnits.ForEach(unit => unit.Select());
+                    }
+                });
+            }
         }
+
         #endregion
     }
 }
