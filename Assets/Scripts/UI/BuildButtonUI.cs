eb2cb6a4fb01363fdb93155a640a17c9f5b36ccd
Ilya Blokh <ilyablokh@gmail.com>
2022-04-17 10:50:51 +0300

    placing building preview

--- Assets/Scripts/UI/BuildButtonUI.cs
+++ Assets/Scripts/UI/BuildButtonUI.cs
@@ -1,6 +1,7 @@
 using Buildings;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 namespace UI
@@ -10,14 +11,25 @@ namespace UI
         [SerializeField] private Button buildButton;
         [SerializeField] private TMP_Text buildCost;
         [SerializeField] private Building building;
+        [SerializeField] private LayerMask floorMask;
 
+        private Camera mainCamera;
         private GameObject buildingPreviewInstance;
+        private Ray ray;
+        private bool isPlacing = false;
 
         private void Start()
         {
+            mainCamera = Camera.main;
             buildButton.image.sprite = building.Icon;
             buildCost.text = building.Price.ToString();
             buildButton.onClick.AddListener(OnBuildClick);
+        }
+
+        private void Update()
+        {
+            if (isPlacing)
+                UpdatePlacing();
         }
 
         private void OnDestroy()
@@ -27,7 +39,40 @@ namespace UI
 
         private void OnBuildClick()
         {
-            Debug.Log("build!");
+            if (isPlacing) StopPlacing();
+            StartPlacing();
+        }
+
+        private void StartPlacing()
+        {
+            isPlacing = true;
+            buildingPreviewInstance = Instantiate(building.PreviewGameobject);
+            buildingPreviewInstance.SetActive(false);
+        }
+
+        private void UpdatePlacing()
+        {
+            ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (Physics.Raycast(ray, out RaycastHit hitInfo, floorMask))
+            {
+                buildingPreviewInstance.transform.position = hitInfo.point;
+                if (!buildingPreviewInstance.activeSelf)
+                    buildingPreviewInstance.SetActive(true);
+                if (Mouse.current.leftButton.wasPressedThisFrame)
+                {
+                    Debug.Log("build here");
+                    StopPlacing();
+                }else if (Mouse.current.rightButton.wasPressedThisFrame)
+                {
+                    StopPlacing();
+                }
+            }
+        }
+
+        private void StopPlacing()
+        {
+            Destroy(buildingPreviewInstance);
+            isPlacing = false;
         }
     }
 }
