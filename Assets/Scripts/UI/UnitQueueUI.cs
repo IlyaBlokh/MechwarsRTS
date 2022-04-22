64d705fa31482f0312c5f655b5ec8d6fdbc4e3fb
Ilya Blokh <ilyablokh@gmail.com>
2022-04-22 11:58:25 +0300

    unit queue UI

---
+++ Assets/Scripts/UI/UnitQueueUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UnitQueueUI : MonoBehaviour
+    {
+        [SerializeField] private Image queueProgressImage;
+        [SerializeField] private TMP_Text queueText;
+    }
+}
